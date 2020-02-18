9b2e15fff647d8dd322c1d3a0edcd0df9adf7876
princehonour <wangzirong@saylooks.com>
2020-02-18 17:48:43 +0800

    增加健康填报页面

---
+++ pages/healthReport/healthReport.js
@@ -0,0 +1,50 @@
+var https = require('../../https/https.js');
+
+Page({
+  data: {
+    healthInfoId: '',
+    healthInfo: {}
+  },
+  //事件处理函数
+  submit: function(event) {
+    let _this = this
+    let action = event.currentTarget.dataset.action
+    https.putRequest('/common/health-statement/quarantine', {
+      action: action,
+      id: _this.data.healthInfoId
+    }, (res) => {
+      wx.navigateTo({
+        url: '../scan/scan'
+      })
+    }, (err) => {})
+  },
+  onLoad: function() {
+    let _this = this
+    const eventChannel = this.getOpenerEventChannel()
+    if (eventChannel && eventChannel.on) {
+      eventChannel.on('healthInfoId', function(data) {
+        https.getRequest('/common/health-statement/detail/' + data.id, null, (res) => {
+          if (res && res.data) {
+            _this.setData({
+              healthInfoId: data.id,
+              healthInfo: res.data
+            })
+          } else {
+            wx.showToast({
+              title: '未查询到健康证信息',
+              icon: 'none'
+            })
+          }
+        }, (err) => {})
+      })
+    }
+  },
+  checkNotNull(value) {
+    if (!value) {
+      return false
+    } else if (JSON.stringify(value) === '{}') {
+      return false
+    }
+    return true
+  }
+})
